6211059160f79cfa39a5144386eed2cf8d48cd67
Hasib Alamyar <hasibalamyar2@gmail.com>
2025-07-28 04:22:08 +0430

    statics linked : base.html and index.html is done

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    # 'school.apps.SchoolConfig'
+    'school'
+
+    # modules(third party apps)
+    # 'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
